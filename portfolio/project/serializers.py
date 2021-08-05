54ce4d4cabe23666892bee320a31a8d1c814942b
albeXL <albexxl27@gmail.com>
2021-08-05 14:39:16 -0400

    Add ProjectInfo serializer

---
+++ portfolio/project/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from project.models import ProjectInfo
+
+
+class ProjectInfoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProjectInfo
+        fields = '__all__'
