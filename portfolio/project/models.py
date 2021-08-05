3b23c92340b5aa57bd232279901c3fd0dc53c5d1
albeXL <albexxl27@gmail.com>
2021-08-05 14:33:03 -0400

    Add ProjectInfo model

--- portfolio/project/models.py
+++ portfolio/project/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProjectInfo(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    github = models.URLField()
+    demo = models.URLField(blank=True)
+    image = models.ImageField(blank=True)
+    show = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Project Info'
+        verbose_name_plural = 'Projects Info'
+
+    def __str__(self):
+        return self.name
