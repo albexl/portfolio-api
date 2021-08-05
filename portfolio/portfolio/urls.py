bf37a5e790646091b5747a5d09f54834ad43c5ea
albeXL <albexxl27@gmail.com>
2021-08-05 14:24:48 -0400

    Initial project setup

---
+++ portfolio/portfolio/urls.py
@@ -0,0 +1,38 @@
+"""portfolio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+openapi_info = openapi.Info(
+    title="PORTFOLIO API",
+    default_version='v1',
+    description="Test description",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="contact@snippets.local"),
+    license=openapi.License(name="BSD License"),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('project/', include('project.urls')),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+]
