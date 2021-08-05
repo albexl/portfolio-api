a780cbfc7ecc2c38260fc298454a739423bbf879
albeXL <albexxl27@gmail.com>
2021-08-05 14:45:27 -0400

    Register ProjectInfo model in Django Admin

--- portfolio/project/admin.py
+++ portfolio/project/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from project.models import ProjectInfo
+
+
+def CustomModelAdmin(model):
+    return type('SubClass' + model.__name__,
+                (admin.ModelAdmin,
+                 ),
+                {'list_display': [x.name for x in model._meta.fields],
+                    'list_select_related': [x.name for x in model._meta.fields if isinstance(x,
+                                                                                             (models.ManyToOneRel,
+                                                                                              models.ForeignKey,
+                                                                                              models.OneToOneField,
+                                                                                              ))],
+                 'search_fields': ['id'],
+                 'list_display_links': ['id'],
+                 })
+
+
+admin.site.register(ProjectInfo, CustomModelAdmin(ProjectInfo))
